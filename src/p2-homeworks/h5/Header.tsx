24f7b9e578151cd685241f196f308b9cdb586998
GrindezeR <valariot@gmail.com>
2021-09-24 21:06:59 +0300

    home-5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from './Header.module.css';
+import arrow from './pages/images/arrow.png';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.wrapper}>
+            <div className={s.navWrapper}>
+                {/*add NavLinks*/}
+                <NavLink activeClassName={s.active} className={s.link} to={PATH.PRE_JUNIOR}>PRE-JUNIOR</NavLink>
+                <NavLink activeClassName={s.active} className={s.link} to={PATH.JUNIOR}>JUNIOR</NavLink>
+                <NavLink activeClassName={s.active} className={s.link} to={PATH.JUNIOR_PLUS}>JUNIOR+</NavLink>
+                <img src={arrow} className={s.icon} width={'30px'} alt={'NavBar'}/>
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import s from './Error404.module.css';
+import {NavLink} from "react-router-dom";
+import error from './images/404.png';
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.wrapper}>
+
+            <div className={s.error}>
+                <span>4</span>
+                <img className={s.image} src={error} alt="404" width={'200px'}/>
+                <span>4</span>
+            </div>
+            <p className={s.text}>
+                The page you are looking <span>NOT FOUND!</span> So sad...
+            </p>
+
+            <NavLink to={'/'} className={s.home}>Return Home</NavLink>
         </div>
     )
 }
